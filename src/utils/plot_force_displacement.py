802b9a1b9286157b7e515352fe4f50140488177a
gabriela <gabriela.gerber@hotmail.de>
2026-04-20 15:33:35 +0200

    structure fd plot code into functions

--- src/utils/plot_force_displacement.py
+++ src/utils/plot_force_displacement.py
@@ -9,58 +9,80 @@ import subprocess
 import pandas as pd
 import matplotlib.pyplot as plt
 
-## User Input
-# Model data
-folder_path = '/home/gabriela/Documents/04_Projects/2026_NonLocal_Damage_Model/02_Code/non-local-damage-model/tests/homCube_C3D10_NL_comp/'
-model_name = 'C3D10_cube_NL.odb'   
-step_name = 'Step-1'      
-out_name = 'RF_history.csv'
-top_nodes = [5,6,7,8,13,14,15,16,22]
-
-# Other input
-utils_path = '/home/gabriela/Documents/04_Projects/2026_NonLocal_Damage_Model/02_Code/non-local-damage-model/src/utils/'
-
 #**************************************************************************************************************
 
 ## Extract data from odb
-odb_path = folder_path + model_name
-out_csv = folder_path + out_name
+def extract_fd_from_odb(folder_path,model_name,out_name):
+    odb_path = folder_path + model_name
+    out_csv = folder_path + out_name
 
-cmd = ["abaqus", "python", utils_path + "extract_RF_from_ODB.py", odb_path, step_name, out_csv]
-result = subprocess.run(cmd)
+    cmd = ["abaqus", "python", utils_path + "extract_RF_from_ODB.py", odb_path, step_name, out_csv]
+    result = subprocess.run(cmd)
 
-if result.returncode != 0:
-        print("Abaqus python data extraction failed:")
-        print(result.stderr)
-else:
-        print("Extraction finished successfully")
+    if result.returncode != 0:
+            print("Abaqus python data extraction failed:")
+            print(result.stderr)
+    else:
+            print("Extraction finished successfully")
 
-## Import results
-df_res = pd.read_csv(out_csv)
+def fd_plot(plotname,f,d):
+    plt.figure()
+    plt.plot(d, f)
+    plt.xlabel('displacement')
+    plt.ylabel('force')
+    plt.savefig(plotname, dpi=300 )
+    plt.show()
+                 
 
-# Ensure numeric colums are numeric
-cols = ['Increment','RF3','U3','Node']
-df_res[cols] = df_res[cols].apply(pd.to_numeric)
+## Create Force-displacement plot
+def create_fd_plot(path_results):
 
-## Calculate reaction force
-results = []
+    ## Import results
+    df_res = pd.read_csv(path_results)
 
-for inc, g in df_res.groupby('Increment'):
+    # Ensure numeric colums are numeric
+    cols = ['Increment','RF3','U3','Node']
+    df_res[cols] = df_res[cols].apply(pd.to_numeric)
 
-    gsel = g[g['Node'].isin(top_nodes)]
+    ## Calculate reaction force
+    results = []
 
-    Fz = gsel['RF3'].sum()
-    Uz = gsel['U3'].mean()
+    for inc, g in df_res.groupby('Increment'):
 
-    results.append([inc, Uz, Fz])
+        gsel = g[g['Node'].isin(top_nodes)]
 
-res = pd.DataFrame(results, columns=['Increment','U3','RF3_sum'])
+        Fz = gsel['RF3'].sum()
+        Uz = gsel['U3'].mean()
 
-## Plot
-plotname = folder_path + model_name[:-3] + "_fd.png"
-plt.figure()
-plt.plot(res['U3'], res['RF3_sum'])
-plt.xlabel('displacement')
-plt.ylabel('force')
-plt.savefig(plotname, dpi=300 )
-plt.show()
+        results.append([inc, Uz, Fz])
+
+    res = pd.DataFrame(results, columns=['Increment','U3','RF3_sum'])
+
+    ## Plot
+    plotname = folder_path + model_name[:-3] + "_fd.png"
+    f = res['RF3_sum']
+    d = res['U3']
+    fd_plot(plotname,f,d)
+    
+
+if __name__ == "__main__":
+    
+    ## User Input
+    # Model data
+    folder_path = '/home/gabriela/Documents/04_Projects/2026_NonLocal_Damage_Model/02_Code/non-local-damage-model/tests/homCube_C3D10_NL_comp/'
+    model_name = 'C3D10_cube_NL.odb'   
+    step_name = 'Step-1'      
+    out_name = 'RF_history.csv'
+    top_nodes = [5,6,7,8,13,14,15,16,22]
+
+    # Other input
+    utils_path = '/home/gabriela/Documents/04_Projects/2026_NonLocal_Damage_Model/02_Code/non-local-damage-model/src/utils/'
+
+    # Extract results
+    extract_fd_from_odb(folder_path,model_name,out_name)
+
+    # Create force-displacement plot
+    path_results = folder_path + out_name
+    create_fd_plot(path_results)
+
+        
